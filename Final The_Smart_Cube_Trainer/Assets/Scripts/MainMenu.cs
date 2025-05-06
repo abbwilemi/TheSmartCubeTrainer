8580497f9543fa429ef51bef9aebf7371a632e18
abbwilemi <william.emilsson@abbgymnasiet.se>
2025-05-06 14:37:51 +0200

    Added comments

--- Final The_Smart_Cube_Trainer/Assets/Scripts/MainMenu.cs
+++ Final The_Smart_Cube_Trainer/Assets/Scripts/MainMenu.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Load Practise Scene
     public void Practise ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Back to Main Menu 
     public void GoBack ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
+    // Exit TheSmartCubeTrainer
     public void Exit ()
     {
         Debug.Log("Exit");
